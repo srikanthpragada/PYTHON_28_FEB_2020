74e749c09a104b073a3cc9ffdbe9495b5dc68cdd
srikanthpragada <srikanthpragada@gmail.com>
2020-03-02 09:28:04 +0530

    02-MAR

---
+++ demo/even.py
@@ -0,0 +1,12 @@
+# Program to display whether a number is even or odd
+
+num = input("Enter a number :")
+num = int(num)   # convert string to int
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+
