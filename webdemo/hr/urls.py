a29c02ff62bffd761d653a03fbca3de892ef8e48
srikanthpragada <srikanthpragada@gmail.com>
2020-04-06 07:58:33 +0530

    06-APR

--- webdemo/hr/urls.py
+++ webdemo/hr/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('welcome/', views.welcome),
     path('index/', views.index),
     path('employees/', views.list_employees),
+    path('countries/', views.list_countries),
+    path('add_employee/', views.add_employee),
 ]
--- webdemo/hr/views.py
+++ webdemo/hr/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import sqlite3
+import requests
 
 
 # Create your views here.
@@ -21,3 +22,32 @@ def list_employees(request):
     emps = cur.fetchall()
     con.close()
     return render(request, 'list_employees.html', {'employees': emps})
+
+
+def list_countries(request):
+    resp = requests.get("https://restcountries.eu/rest/v2/all")
+    countries = resp.json()
+    countries = sorted(countries, key=lambda c: c['population'], reverse=True)
+    # Calculate total population
+    total = 0
+    return render(request, 'list_countries.html',
+                  {'countries': countries, 'total_population': total})
+
+
+def add_employee(request):
+    if 'fullname' in request.GET:
+        # process data
+        fullname = request.GET['fullname']
+        job = request.GET['job']
+        salary = request.GET['salary']
+
+        # Connect to db and insert into EMPLOYEES table
+        con = sqlite3.connect(r"c:\classroom\feb28\hr.db")
+        cur = con.cursor()
+        cur.execute("insert into employees (fullname,salary,job) values(?,?,?)",
+                    (fullname, salary, job))
+        con.commit()  # Commit transaction / Save changes
+        con.close()
+        return redirect("/hr/employees")
+    else:
+        return render(request, 'add_employee.html')
